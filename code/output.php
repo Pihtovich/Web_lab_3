fc96b9f74417585cebe1ad10169e9e7d6f5ea737
Pihtovich <86502038+Pihtovich@users.noreply.github.com>
2024-10-18 20:28:19 +0200

    Forget to add outputfile for work lab :( (already added)

---
+++ code/output.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if (isset($_SESSION['user']))
+{
+    echo "Фамилия: " . $_SESSION['user']['surname'] . "<br>";
+    echo "Имя: " . $_SESSION['user']['name'] . "<br>";
+    echo "Возраст: " . $_SESSION['user']['age'];
+}
+?>
