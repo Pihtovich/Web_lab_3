ad2b445f052eb0331d4b54c203eeca75b66ed0a0
Pihtovich <86502038+Pihtovich@users.noreply.github.com>
2024-10-18 20:57:22 +0200

    Made normal markup in task2

--- code/task2.php
+++ code/task2.php
@@ -14,17 +14,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     <title>Ввод данных</title>
 </head>
 <body>
-<h2>Введите ваши данные:</h2>
-<form method="post">
-    label for="name">Имя:</label>
-    <input type="text" id="name" name="name"><br><br>
-    <label for="age">Возраст:</label>
-    <input type="text" id="age" name="age"><br><br>
-    <label for="salary">Зарплата:</label>
-    <input type="text" id="salary" name="salary"><br><br>
-    <label for="education">Образование:</label>
-    <input type="text" id="education" name="education"><br><br>
-    <input type="submit" value="Сохранить">
-</form>
+    <h2>Введите ваши данные:</h2>
+    <form method="post">
+        <label for="name">Имя:</label>
+        <input type="text" id="name" name="name"><br><br>
+
+        <label for="age">Возраст:</label>
+        <input type="text" id="age" name="age"><br><br>
+
+        <label for="salary">Зарплата:</label>
+        <input type="text" id="salary" name="salary"><br><br>
+
+        <label for="education">Образование:</label>
+        <input type="text" id="education" name="education"><br><br>
+
+        <input type="submit" value="Сохранить">
+    </form>
 </body>
 </html>
