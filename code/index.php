dc65ba549c8ea25bb5b1ab2c3620d173a1fbd8f6
Pihtovich <86502038+Pihtovich@users.noreply.github.com>
2024-10-18 20:07:10 +0200

    Complete task1.b + screen2

--- code/index.php
+++ code/index.php
@@ -1,11 +1,28 @@
 <?php
-
+//1.a
 $str = 'ahb acb aeb aeeb adcb axeb';
 $pattern = '/a..b/';
 preg_match_all($pattern, $str, $matches);
 
 echo "Найденные строки:\n";
-
-foreach ($matches[0] as $match) {
-    echo $match . "\n";
+foreach ($matches[0] as $match)
+{
+    echo $match."\n";
 }
+echo "<br>";
+echo "\n";
+
+//1.b
+$input = 'a4b2c3';
+echo "Строка до преобразования: ".$input."\n";
+echo "<br>";
+echo "\n";
+$regex = '/(\d+)/';
+
+$replace = function ($matches)
+{
+    return pow($matches[1], 3);
+};
+
+$output = preg_replace_callback($regex, $replace, $input);
+echo "Строка после преобразования: ".$output."\n";
