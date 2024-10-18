1c98bc3791a4685697e0e502e7eea61fb587fe22
Pihtovich <86502038+Pihtovich@users.noreply.github.com>
2024-10-18 20:03:12 +0200

    Complete task1.a + screen1

--- code/index.php
+++ code/index.php
@@ -1,2 +1,11 @@
 <?php
-echo phpinfo();
+
+$str = 'ahb acb aeb aeeb adcb axeb';
+$pattern = '/a..b/';
+preg_match_all($pattern, $str, $matches);
+
+echo "Найденные строки:\n";
+
+foreach ($matches[0] as $match) {
+    echo $match . "\n";
+}
