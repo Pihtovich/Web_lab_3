901d88478684d23e79dccac9005cec7d59ce3006
Pihtovich <86502038+Pihtovich@users.noreply.github.com>
2024-10-18 20:50:41 +0200

    Added task3 + screens

--- code/output.php
+++ code/output.php
@@ -1,10 +1,23 @@
 <?php
-session_start();
+function redirectToHome()
 if(isset($_SESSION['userData']))
-    echo '<ul>';
-    foreach ($_SESSION['userData'] as $key => $value)
-    {
-        echo "<li>$key: $value</li>";
-    }
-    echo '</ul>';
+{
+    header("Location: task3.php");
+    exit();
 }
+
+if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description']))
+{
+    redirectToHome();
+}
+
+$category = $_POST['category'];
+$email = $_POST['email'];
+$title = $_POST['title'];
+$desc = $_POST['description'];
+$category_folder = "./categories/$category";
+$file_path = "$category_folder/$title.txt";
+$content = "$email\n$title\n$desc";
+file_put_contents($file_path, $content);
+redirectToHome();
+?>
